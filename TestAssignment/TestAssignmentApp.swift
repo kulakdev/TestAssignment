4b61013547d4e49bac75da5b53d9201272a92b0d
jke <constantinekulak@proton.me>
2023-08-01 09:32:30 +0300

    Initial Commit

---
+++ TestAssignment/TestAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAssignmentApp.swift
+//  TestAssignment
+//
+//  Created by Constantine Kulak on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
