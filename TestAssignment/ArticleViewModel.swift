a131fde039741a51ff5c94ef63bb265a1e265523
jke <constantinekulak@proton.me>
2023-08-01 11:06:18 +0300

    Created the View and ViewModels that can fetch the data required

---
+++ TestAssignment/ArticleViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  ArticleViewModel.swift
+//  TestAssignment
+//
+//  Created by Constantine Kulak on 01.08.2023.
+//
+
+import Foundation
+
+class ArticleViewModel: ObservableObject {
+    @Published private(set) var articles = [Article]()
+    
+    func fetchNews(query: String){
+        
+        var urlComponents = URLComponents()
+            urlComponents.scheme = "https"
+            urlComponents.host = "newsapi.org"
+            urlComponents.path = "/v2/everything"
+            
+            urlComponents.queryItems = [
+                URLQueryItem(name: "q", value: query),
+                URLQueryItem(name: "apiKey", value: "f1ec47c0a4a84ebe99aaf50d447f5c64")
+            ]
+        
+        guard let url = urlComponents.url else {
+            print("invalid url")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        
+        request.addValue("f1ec47c0a4a84ebe99aaf50d447f5c64", forHTTPHeaderField: "X-Auth-Token")
+        request.httpMethod = "GET"
+        
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+                // handle error
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                
+                // check if status code == 200?
+                if let httpResponse = response as? HTTPURLResponse {
+                    if httpResponse.statusCode == 200 {
+                        if let data = data {
+                            do {
+                                let decoder = JSONDecoder()
+                                decoder.dateDecodingStrategy = .iso8601
+                                
+                                let articleResponse = try decoder.decode(ArticleResponse.self, from: data)
+                                
+                                let articles = articleResponse.articles
+                                
+                                print("Articles count: \(articles.count)")
+                                DispatchQueue.main.async {
+                                    self.articles = articleResponse.articles
+                                }
+                            } catch {
+                                print("Error decoding JSON: \(error)")
+                            }
+                        }
+                    } else {
+                        // status not 200
+                        print("HTTP status code: \(httpResponse.statusCode)")
+                    }
+                }
+            }
+        dataTask.resume()
+        
+    }
+}
--- TestAssignment/ContentView.swift
+++ TestAssignment/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var q = ""
+    @StateObject private var viewModel = ArticleViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                TextField("Enter search parameter", text: $q)
+                Button("Search"){
+                    viewModel.fetchNews(query: q)
+                }
+                List(viewModel.articles, id: \.title) { item in
+                    VStack {
+                        Text(item.title)
+                            .font(.headline)
+                        Text(item.author ?? "author unknown")
+                    }
+                }
+            }
         }
         .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
