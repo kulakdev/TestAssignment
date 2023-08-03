bcca97d3f8bee6165fc7820ea973d5108135785c
gitConstantineKulak <constantinekulak@proton.me>
2023-08-03 22:42:50 +0300

    moved all the views into Views folder

---
+++ TestAssignment/Views/NewsListView.swift
@@ -0,0 +1,15 @@
+//
+//  NewsListView.swift
+//  TestAssignment
+//
+//  Created by Constantine Kulak on 03.08.2023.
+//
+
+import Foundation
+
+
+struct NewsListView {
+    var body: some View{
+        
+    }
+}
